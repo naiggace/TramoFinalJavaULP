9bc4fd15b00032b7280ad83d1f71da7b13d63429
Arancibia Leiria Leandro Nicanor <Nicanor95@users.noreply.github.com>
2023-10-02 12:56:26 -0300

    esqueleto inquilinodatos

--- src/trabajofinaljavaulp/datos/InquilinoDatos.java
+++ src/trabajofinaljavaulp/datos/InquilinoDatos.java
@@ -1,10 +1,32 @@
 
 package trabajofinaljavaulp.datos;
 
+import trabajofinaljavaulp.entidades.Inquilino;
+
 /**
  *
  * @author nikan
  */
 public class InquilinoDatos {
+    public void agregarInquilino(Inquilino i) {
+        //TODO
+    }
     
+    public void bajaInquilino(int i) {
+        //TODO
+    }
+    
+    public Inquilino buscarInquilinoID(int id) {
+        //TODO
+        return new Inquilino();
+    }
+    
+    public Inquilino buscarInquilinoDNI(int dni) {
+        //TODO
+        return new Inquilino();
+    }
+    
+    public void modificarDatos(Inquilino i) {
+    
+    }
 }
