c412bdff2497fa5701e7fa492083f17192d46c46
Arancibia Leiria Leandro Nicanor <Nicanor95@users.noreply.github.com>
2023-10-02 11:38:44 -0300

    Esqueletos

---
+++ src/trabajofinaljavaulp/entidades/Inquilino.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabajofinaljavaulp.entidades;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author nikan
+ */
+public class Inquilino {
+    private String nombre;
+    private String apellido;
+    private int dni;
+    private int id;
+    private ArrayList<Inmueble> propiedades;
+    
+}
