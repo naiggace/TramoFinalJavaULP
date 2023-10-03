2bfe55a17456ccf301ce27d78fefa71c02b1b297
IgnacioCarida <ignacio.carida@gmail.com>
2023-10-03 11:25:47 -0300

    Class Inquilino
    modificación de atributos

--- src/trabajofinaljavaulp/entidades/Inquilino.java
+++ src/trabajofinaljavaulp/entidades/Inquilino.java
@@ -12,28 +12,44 @@ public class Inquilino {
     private String apellido;
     private int dni;
     private int id;
+    private String direccion;
+    private String email;
+    private int telefono;
+    private boolean estado;
     private ArrayList<Inmueble> propiedades;
 
-    public Inquilino(String nombre, String apellido, int dni, int id, ArrayList<Inmueble> propiedades) {
+    public Inquilino(String nombre, String apellido, int dni, int id, String direccion, String email, int telefono, boolean estado, ArrayList<Inmueble> propiedades) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.dni = dni;
         this.id = id;
+        this.direccion = direccion;
+        this.email = email;
+        this.telefono = telefono;
+        this.estado = estado;
         this.propiedades = propiedades;
     }
 
-    public Inquilino(String nombre, String apellido, int dni, ArrayList<Inmueble> propiedades) {
+    public Inquilino(String nombre, String apellido, int dni, String direccion, String email, int telefono) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.dni = dni;
-        this.propiedades = propiedades;
+        this.direccion = direccion;
+        this.email = email;
+        this.telefono = telefono;
     }
 
-    public Inquilino(String nombre, String apellido, int dni) {
+    public Inquilino(String nombre, String apellido, int dni, String direccion, String email, int telefono, boolean estado) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.dni = dni;
+        this.direccion = direccion;
+        this.email = email;
+        this.telefono = telefono;
+        this.estado = estado;
     }
+
+    
 
     public Inquilino() {
     }
@@ -78,8 +94,42 @@ public class Inquilino {
         this.propiedades = propiedades;
     }
 
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+
     @Override
     public String toString() {
-        return "Inquilino{" + "nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni + '}';
+        return "Inquilino{" + "nombre=" + nombre + ", apellido=" + apellido + 
+                ", dni=" + dni +"Email= "+email+" Telefono= "+telefono+ '}';
     }
 }
