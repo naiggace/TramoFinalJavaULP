3cdc33f00b1a6f5f4573432d0595450d5dd23f39
Ben <100096813+BenRomeroArg@users.noreply.github.com>
2023-10-04 10:01:14 -0300

    vista inmobiliaria

--- src/trabajofinaljavaulp/Vistas/InmobiliariaView.java
+++ src/trabajofinaljavaulp/Vistas/InmobiliariaView.java
@@ -14,7 +14,36 @@ public class InmobiliariaView extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jMenu1 = new javax.swing.JMenu();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu2 = new javax.swing.JMenu();
+        jMenu5 = new javax.swing.JMenu();
+        jMenu6 = new javax.swing.JMenu();
+        jMenu3 = new javax.swing.JMenu();
+        jMenu4 = new javax.swing.JMenu();
+
+        jMenu1.setText("jMenu1");
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Inmobiliaria 22");
+
+        jMenu2.setText("Ingresos");
+
+        jMenu5.setText("Propietarios");
+        jMenu2.add(jMenu5);
+
+        jMenu6.setText("Inquilinos");
+        jMenu2.add(jMenu6);
+
+        jMenuBar1.add(jMenu2);
+
+        jMenu3.setText("Inmuebles");
+        jMenuBar1.add(jMenu3);
+
+        jMenu4.setText("Consultas");
+        jMenuBar1.add(jMenu4);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -24,7 +53,7 @@ public class InmobiliariaView extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 277, Short.MAX_VALUE)
         );
 
         pack();
@@ -66,5 +95,12 @@ public class InmobiliariaView extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu3;
+    private javax.swing.JMenu jMenu4;
+    private javax.swing.JMenu jMenu5;
+    private javax.swing.JMenu jMenu6;
+    private javax.swing.JMenuBar jMenuBar1;
     // End of variables declaration//GEN-END:variables
 }
