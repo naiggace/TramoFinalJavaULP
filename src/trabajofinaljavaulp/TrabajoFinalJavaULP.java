dc84b3abb7461178950005dad9d54737eb1e6db3
Ben <100096813+BenRomeroArg@users.noreply.github.com>
2023-10-06 12:44:02 -0300

    prueba de metodos v.2

--- src/trabajofinaljavaulp/TrabajoFinalJavaULP.java
+++ src/trabajofinaljavaulp/TrabajoFinalJavaULP.java
@@ -1,11 +1,14 @@
 package trabajofinaljavaulp;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.time.LocalDate;
 import trabajofinaljavaulp.datos.AlquilerDatos;
 import trabajofinaljavaulp.datos.Conexion;
 import trabajofinaljavaulp.datos.InmuebleDatos;
 import trabajofinaljavaulp.datos.InquilinoDatos;
 import trabajofinaljavaulp.datos.PropietarioDatos;
+import trabajofinaljavaulp.entidades.Alquiler;
 import trabajofinaljavaulp.entidades.Inmueble;
 import trabajofinaljavaulp.entidades.Inquilino;
 import trabajofinaljavaulp.entidades.Propietario;
@@ -18,27 +21,32 @@ public class TrabajoFinalJavaULP {
 
     public static void main(String[] args) {
 
-
         Connection con = Conexion.conectar();
-        
+
         PropietarioDatos prop = new PropietarioDatos();
-        
-        Propietario p = prop.buscarId(91,true);
+
+        Propietario p = prop.buscarId(92, true);
         System.out.println(p.toString());
         InmuebleDatos inmD = new InmuebleDatos();
-        
-        Inmueble inm = inmD.buscar(60,true);
+
+        Inmueble inm = inmD.buscar(61, true);
         System.out.println(inm.toString());
-        
+
         InquilinoDatos inqD = new InquilinoDatos();
-        
-        Inquilino cliente = inqD.buscarDNI(46497062, true);
+
+        Inquilino cliente = inqD.buscarDNI(45267765, true);
         System.out.println(cliente.toString());
-        
-        AlquilerDatos alq = new AlquilerDatos();
-        
-        System.out.println(alq.listar(true));
-        
+
+        Alquiler al;
+
+        Date fechaI = Date.valueOf(LocalDate.of(2022, 06, 05));
+        Date fechaF = Date.valueOf(LocalDate.of(2024, 07, 25));
+        al = new Alquiler(fechaI, fechaF, 149000.99, "39-1228292-9", "Martinez Jose", 40304040, "Coca Cola", true, inm, cliente);
+//        //Error al listar alquileres
+        AlquilerDatos.agregar(al);
+//        for (Alquiler a : AlquilerDatos.listar(true)) {
+//            System.out.println(a.toString());
+//        }
 
     }
 }
--- src/trabajofinaljavaulp/datos/AlquilerDatos.java
+++ src/trabajofinaljavaulp/datos/AlquilerDatos.java
@@ -7,38 +7,63 @@ import trabajofinaljavaulp.entidades.Alquiler;
 import trabajofinaljavaulp.entidades.Inmueble;
 import trabajofinaljavaulp.entidades.Inquilino;
 
-
 public class AlquilerDatos {
+
     private static Connection con = Conexion.conectar();
-    
+
     /**
      * Agregar <b>Alquiler</b> a la base de datos
-     * 
+     *
      * @param a <i>Alquiler</i> a agregar.
      * @see Alqulier
      */
     public static void agregar(Alquiler a) {
-    //TODO
+        String sql = "INSERT INTO `alquiler`(`idAlquiler`, `idInquilino`, `idInmueble`, `CuitInquilino`, `lugarTrabajo`, `nombreGarante`, `dniGarante`, `fechaInicio`, `fechaFin`, `estado`, `montoAlquiler`) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+        try (PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            ps.setInt(1, a.getId());
+            ps.setInt(2, a.getInquilino().getId());
+            ps.setInt(3, a.getInmueble().getId());
+            ps.setString(4, a.getCuit());
+            ps.setString(5, a.getLugarTrabajo());
+            ps.setString(6, a.getNombreGarante());
+            ps.setInt(7, a.getDniGarante());
+            ps.setDate(8, a.getFechaInicio());
+            ps.setDate(9, a.getFechaFin());
+            ps.setBoolean(10, a.isEstado());
+            ps.setDouble(11, a.getMonto());
+            ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+
+            if (rs.next()) {
+                a.setId(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Alquiler añadido con exito, ID: " + a.getId(), "Exito", JOptionPane.INFORMATION_MESSAGE);
+            }
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al agregar alquiler: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+
     }
-    
+
     /**
      * Regresa un resultado dependiendo de si el <b>Alquiler</b> existe.
-     * 
+     *
      * @param id <i>int</i> el Id a chequear.
      * @return  <b>0</b> El Alquiler no existe en la base de datos.<br>
-     *          <b>1</b> El Alquiler existe y esta activo.<br>
-     *          <b>2</b> El Alquiler existe y no esta activo.
+     * <b>1</b> El Alquiler existe y esta activo.<br>
+     * <b>2</b> El Alquiler existe y no esta activo.
      */
     public static int existe(int id) {
         String sql = "SELECT estado FROM alquiler WHERE idAlquiler = ?";
         int existe = 0;
-        
+
         try (PreparedStatement ps = con.prepareStatement(sql)) {
             ps.setInt(1, id);
-            
+
             //Ejecutamos el PreparedStatement y guardamos el resultado.
             ResultSet rs = ps.executeQuery();
-            
+
             if (rs.next()) { //El Alquiler con ese id existe.
                 if (rs.getInt("estado") == 1) { //Está activo
                     existe = 1;
@@ -46,34 +71,36 @@ public class AlquilerDatos {
                     existe = 2;
                 }
             }
-            
+
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error al acceder a alquiler: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
         }
-        
+
         return existe;
     }
-    
+
     /**
      * Da de baja un <b>Alquiler</b>.
+     *
      * @param id <i>int</i> correspondiente al idAlquiler.
      */
     public static void baja(int id) {
-    //TODO
+        //TODO
     }
-    
+
     /**
      * Da de alta un <b>Alquiler</b>.
+     *
      * @param id <i>int</i> correspondiente al idAlquiler.
      */
     public static void alta(int id) {
-    //TODO
+        //TODO
     }
-    
+
     /**
      * Regresa el <b>Alquiler</b> con el idAlquiler correspondiente.<br>
      * No nos interesa el estado.
-     * 
+     *
      * @param id <i>int</i> corresponde a idAlquiler.
      * @return <b>Alquiler</b>
      * @see Alquiler
@@ -82,10 +109,10 @@ public class AlquilerDatos {
         //TODO
         return new Alquiler();
     }
-    
+
     /**
      * Listar todos los alquileres con el correspondiente estado
-     * 
+     *
      * @param estado <i>boolean</i> Estado de los alquileres.
      * @return <b>ArrayList</b> Con alquileres.
      * @see Alquiler
@@ -93,39 +120,39 @@ public class AlquilerDatos {
     public static ArrayList<Alquiler> listar(boolean estado) {
         String sql = "SELECT * FROM alquiler WHERE estado = ?";
         ArrayList<Alquiler> alquileres = new ArrayList();
-        
+
         try (PreparedStatement ps = con.prepareStatement(sql)) {
             ps.setBoolean(1, estado);
-            
+
             //Ejecutamos el PreparedStatment y guardamos el resultado en un ResultSet.
             ResultSet rs = ps.executeQuery();
-            
+
             //Recorremos el ResultSet guardando los resultados en el ArrayList alquileres
             while (rs.next()) {
                 // Encontramos el Inmueble
                 int id = rs.getInt("idInmueble");
-                Inmueble inmueble = InmuebleDatos.buscar(rs.getInt(id), 
+                Inmueble inmueble = InmuebleDatos.buscar(rs.getInt(id),
                         InmuebleDatos.estado(id) == 1
                 );
-                
+
                 // Encontramos el Inquilino
                 id = rs.getInt("idInquilino");
-                Inquilino inquilino = InquilinoDatos.buscarID(id, 
+                Inquilino inquilino = InquilinoDatos.buscarID(id,
                         InquilinoDatos.existe(id) == 1
                 );
-                
+
                 //Agregamos el alquiler a la lista
                 alquileres.add(new Alquiler(
-                        rs.getInt("idAlquiler"), 
-                        rs.getDate("fechaInicio"), 
-                        rs.getDate("fechaFin"), 
-                        rs.getDouble("monto"), 
-                        rs.getString("cuitInquilino"), 
-                        rs.getString("nombreGarante"), 
-                        rs.getInt("dniGarante"), 
-                        rs.getString("lugarTrabajo"), 
-                        rs.getBoolean("estado"), 
-                        inmueble, 
+                        rs.getInt("idAlquiler"),
+                        rs.getDate("fechaInicio"),
+                        rs.getDate("fechaFin"),
+                        rs.getDouble("monto"),
+                        rs.getString("cuitInquilino"),
+                        rs.getString("nombreGarante"),
+                        rs.getInt("dniGarante"),
+                        rs.getString("lugarTrabajo"),
+                        rs.getBoolean("estado"),
+                        inmueble,
                         inquilino)
                 );
             }
@@ -136,5 +163,5 @@ public class AlquilerDatos {
 
         return alquileres;
     }
-    
+
 }
