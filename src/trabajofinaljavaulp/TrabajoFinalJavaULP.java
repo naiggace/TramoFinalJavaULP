7602aac19c04f9a88caacf5701bb342933602ea7
naiggace <naiggace@hotmail.com>
2023-10-09 11:36:55 -0300

    Update TrabajoFinalJavaULP.java

--- src/trabajofinaljavaulp/TrabajoFinalJavaULP.java
+++ src/trabajofinaljavaulp/TrabajoFinalJavaULP.java
@@ -45,11 +45,38 @@ public class TrabajoFinalJavaULP {
         al = new Alquiler(fechaI, fechaF, 149000.99, "39-1228292-9", "Martinez Jose", 40304040, "Coca Cola", true, inm, cliente);
 
         AlquilerDatos.agregar(al);
-        
+
         ArrayList<Alquiler> ldalquileres = AlquilerDatos.listar(true);
         for (Alquiler a : ldalquileres) {
             System.out.println(a.toString());
         }
 
+        // Connection con = Conexion.conectar();
+        //  PropietarioDatos prop = new PropietarioDatos();
+        // Crear un objeto Propietario
+        // Propietario propietario = new Propietario(120, 1211, "GOMEZ", "Lucio", "correo@example.com", 123456789);
+        // Crear un objeto Inmueble con el propietario
+        //Inmueble inmueble = new Inmueble("holanda 21", propietario, "local", 312, 321, true);
+        // Crear objetos para acceder a la capa de datos
+        //  PropietarioDatos propietarioDatos = new PropietarioDatos();
+        // PropietarioDatos.baja(116);
+        //PropietarioDatos.agregar(propietario);
+        // Agregar el inmueble a la base de datos
+        //    Propietario p = prop.buscarId(92, true);
+        //   System.out.println(p.toString());
+        //  InmuebleDatos inmD = new InmuebleDatos();
+        //    Inmueble inm = inmD.buscar(61, true);
+        //   System.out.println(inm.toString());
+        //  InquilinoDatos inqD = new InquilinoDatos();
+        //Inquilino cliente = inqD.buscarDni(45267765, true);
+        //System.out.println(cliente.toString());
+        //Alquiler al;
+        //    Date fechaI = Date.valueOf(LocalDate.of(2022, 06, 05));
+        //    Date fechaF = Date.valueOf(LocalDate.of(2024, 07, 25));
+        //   al = new Alquiler(fechaI, fechaF, 149000.99, "39-1228292-9", "Martinez Jose", 40304040, "Coca Cola", true, inm, cliente);
+        //   AlquilerDatos.agregar(al);
+        //   ArrayList<Alquiler> ldalquileres = AlquilerDatos.listar(true);
+        //   for (Alquiler a : ldalquileres) {
+        //       System.out.println(a.toString());
     }
 }
