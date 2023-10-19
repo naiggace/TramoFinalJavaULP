0371522f05173ca9ea21d89aafc9299e29a1e4c8
Arancibia Leiria Leandro Nicanor <Nicanor95@users.noreply.github.com>
2023-10-19 12:23:06 -0300

    Eliminar direccion de PropietarioView, no tenemos esa informacion

--- src/trabajofinaljavaulp/Vistas/PropietarioView.java
+++ src/trabajofinaljavaulp/Vistas/PropietarioView.java
@@ -49,9 +49,7 @@ public class PropietarioView extends javax.swing.JInternalFrame {
         jLabel1 = new javax.swing.JLabel();
         jtDni = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jtDireccion = new javax.swing.JTextField();
         jtEmail = new javax.swing.JTextField();
         jtTelefono = new javax.swing.JTextField();
         jtApellido = new javax.swing.JTextField();
@@ -74,8 +72,6 @@ public class PropietarioView extends javax.swing.JInternalFrame {
 
         jLabel3.setText("Apellido:");
 
-        jLabel4.setText("Direccion:");
-
         jLabel2.setText("Nombre:");
 
         jtTelefono.addActionListener(new java.awt.event.ActionListener() {
@@ -97,7 +93,6 @@ public class PropietarioView extends javax.swing.JInternalFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel2)
                     .addComponent(jLabel3)
-                    .addComponent(jLabel4)
                     .addComponent(jLabel5)
                     .addComponent(jLabel6)
                     .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -105,7 +100,6 @@ public class PropietarioView extends javax.swing.JInternalFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jtNombre, javax.swing.GroupLayout.DEFAULT_SIZE, 234, Short.MAX_VALUE)
                     .addComponent(jtApellido)
-                    .addComponent(jtDireccion)
                     .addComponent(jtDni, javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jtEmail, javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jtTelefono, javax.swing.GroupLayout.Alignment.TRAILING))
@@ -128,17 +122,13 @@ public class PropietarioView extends javax.swing.JInternalFrame {
                     .addComponent(jLabel3))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jtDireccion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel4))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jtEmail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel5))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jtTelefono, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel6))
-                .addContainerGap())
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         jbNuevo.setText("Nuevo");
@@ -214,7 +204,6 @@ public class PropietarioView extends javax.swing.JInternalFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel1;
@@ -222,7 +211,6 @@ public class PropietarioView extends javax.swing.JInternalFrame {
     private javax.swing.JPanel jPanel3;
     private javax.swing.JButton jbNuevo;
     private javax.swing.JTextField jtApellido;
-    private javax.swing.JTextField jtDireccion;
     private javax.swing.JTextField jtDni;
     private javax.swing.JTextField jtEmail;
     private javax.swing.JTextField jtNombre;
